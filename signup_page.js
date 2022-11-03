896e9dbf47de20acd7da331e0fb6514a799b607a
sung6770 <114071008+sung6770@users.noreply.github.com>
2022-11-03 23:16:49 +0900

    fix: 로그인 페이지 수정

--- signup_page.js
+++ signup_page.js
@@ -5,51 +5,50 @@ function checkEmail() {
 
     if ((email.value).match(regExp) == null) {
         informWrongEmail.textContent = '잘못된 이메일 형식입니다!';
-        email.value = null;
-        return true;
     }
     else informWrongEmail.textContent = '';
+    if(email.value.length ==0) informWrongEmail.textContent = '';
 }
 
 function checkPassword() {
     let inputPassword = document.getElementById('input-password');
     let checkPassword = document.getElementById('check-password');
     let informWrongPassword = document.getElementById('wrong-password')
-    if (inputPassword.value != checkPassword.value) {
+    if (inputPassword.value != checkPassword.value && inputPassword.length!=0) {
         informWrongPassword.textContent = '비밀번호가 일치하지 않습니다!';
-        checkPassword.value = null;
     }
     else informWrongPassword.textContent = '비밀번호가 일치합니다!';
+    if(checkPassword.value.length == 0) informWrongPassword.textContent = '';
 }
 
 let studentNumber = document.getElementById('student-number');
+let informWrongStudentNumber = document.getElementById('wrong-studentnumber');
 
 function checkNumber() {
     let number = studentNumber.value;
-    let informWrongStudentNumber = document.getElementById('wrong-studentnumber');
     if (number.length != 10) {
         informWrongStudentNumber.textContent = '학번이 잘못되었습니다!';
-        studentNumber.value = null;
     }
     else informWrongStudentNumber.textContent = '';
+    if(number.length ==0) informWrongStudentNumber.textContent = '';
 }
-
-studentNumber.addEventListener('change', checkMajor);
 
 function checkMajor() {
     let number = studentNumber.value;
     let major = document.getElementById('major');
-    if (number.slice(4, 7) == 136) major.value = '컴퓨터공학부'
-    else if (number.slice(4, 7) == 120) major.value = '기계공학부'
-    else if (number.slice(4, 7) == 140) major.value = '메카트로닉스공학부'
-    else if (number.slice(4, 7) == 161) major.value = '전자정보통신공학부'
-    else if (number.slice(4, 7) == 151) major.value = '디자인공학부'
-    else if (number.slice(4, 7) == 172) major.value = '건축공학부'
-    else if (number.slice(4, 7) == 174) major.value = '에너지신소재공학부'
-    else if (number.slice(4, 7) == 180) major.value = '산업경영학부'
-    else if (number.slice(4, 7) == 185) major.value = '고용서비스정책학부'
+    let majorNum = number.slice(4,7);
+    if (majorNum == 136) major.value = '컴퓨터공학부'
+    else if (majorNum == 120) major.value = '기계공학부'
+    else if (majorNum == 140) major.value = '메카트로닉스공학부'
+    else if (majorNum == 161) major.value = '전자정보통신공학부'
+    else if (majorNum == 151) major.value = '디자인공학부'
+    else if (majorNum == 172) major.value = '건축공학부'
+    else if (majorNum == 174) major.value = '에너지신소재공학부'
+    else if (majorNum == 180) major.value = '산업경영학부'
+    else if (majorNum == 185) major.value = '고용서비스정책학부'
     else {
         major.value = '학번을 다시 입력하십시오';
+        informWrongStudentNumber.textContent = '학번이 잘못되었습니다!';
     }
 }
 
@@ -61,6 +60,7 @@ function insertHyphen() {
         phoneNumber.value = phoneNumber.value.slice(0, 3) + '-' + phoneNumber.value.slice(3, 7) + '-' + phoneNumber.value.slice(7, 11);
         informWrongphonenumber.textContent = '';
     }
+    if(phoneNumber.value.length == 0) informWrongphonenumber.textContent = "";
 }
 
 let closebtn = document.getElementById('btn-close');
